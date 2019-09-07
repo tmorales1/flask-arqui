71a5e0514f18ee8348bc54957e465d39f1455481
Tomas <tmorales1@uc.cl>
2019-09-06 21:40:04 -0400

    ultimo

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ def add_msg(msgs, msg):
     else:
         del msgs[0]
         msgs.append(msg)
+    print(msgs, 'HOLA')
     pickle.dump(msgs, open('data.p', 'wb'))
 
-def get_msgs(offset=0, per_page=5):
-    print(msgs[offset: offset + per_page])
-    return msgs[offset: offset + per_page]
+def get_msgs(msgs, offset=0, per_page=5):
+    if offset + per_page < len(msgs):
+        final = offset*5 + per_page
+    else:
+        final = len(msgs)
+    print(msgs[offset*5: final])
+    return msgs[offset*5: final]
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    try:
-        msgs = pickle.load(open('data.p', 'rb'))
-        print(msgs)
-    except:
-        msgs = []
     page, per_page, offset = get_page_args(page_parameter='page',
                                            per_page_parameter='per_page')
     print(page, per_page, offset)
     total = len(msgs)
-    pagination_msgs = get_msgs(offset=page-1, per_page=5)
+    pagination_msgs = get_msgs(msgs, offset=page-1, per_page=5)
     pagination = Pagination(page=page, per_page=5, total=total,
                             css_framework='bootstrap4')
     return render_template('home.html', msgs=pagination_msgs,
@@ -43,16 +43,13 @@ def home():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    try:
-        msgs = pickle.load(open('data.p', 'rb'))
-    except:
-        msgs = []
     text = request.form['text']
     add_msg(msgs, (text, datetime.datetime.now()))
     page, per_page, offset = get_page_args(page_parameter='page',
                                            per_page_parameter='per_page')
     total = len(msgs)
-    pagination_msgs = get_msgs(offset=page-1, per_page=5)
+    print(len(msgs))
+    pagination_msgs = get_msgs(msgs, offset=page-1, per_page=5)
     pagination = Pagination(page=page, per_page=5, total=total,
                             css_framework='bootstrap4')
     return render_template('home.html', msgs=pagination_msgs,
